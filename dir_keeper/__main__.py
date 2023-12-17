333f3c3a89d347c274eb7aa3c64006e74095e5b4
oratiek <oratiek522@gmail.com>
2023-12-17 22:16:15 +0900

    動作確認済み

--- dir_keeper/__main__.py
+++ dir_keeper/__main__.py
@@ -1,67 +1,47 @@
-from utils import *
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 import argparse
 import time
 import os
 import threading 
-#from gui_dialog import open_dialog
+from gui_dialog import select_dir
 import flet as ft
+from multiprocessing import Process
+import tkinter as tk
+from tkinter import filedialog, Tk
 
-def open_dialog(page: ft.Page):
-    def pick_files_result(e: ft.FilePickerResultEvent):
-        print(e.path)
-
-    pick_files_dialog = ft.FilePicker(on_result=pick_files_result)
-    selected_files = ft.Text()
-
-    page.overlay.append(pick_files_dialog)
-
-    page.add(
-        ft.Row(
-            [
-                ft.ElevatedButton(
-                    "Choose Directory",
-                    icon = ft.icons.UPLOAD_FILE,
-                    on_click = lambda _:pick_files_dialog.get_directory_path()
-                ),
-                selected_files,
-            ]
-        )
-    )
+def _open(root):
+    path = filedialog.askdirectory()
+    print(path)
 
 class EventHandler(FileSystemEventHandler): # should be out of this file
-    def on_any_event(self, e):
-        pass
-
-    def on_modified(self, e):
-        pass
+    def __init__(self, root):
+        self.root = root
 
     def on_created(self, e):
         # check path
         new_file_path = e.src_path
-        print(f"new file createdw:{new_file_path}")
+        print(f"new file create:{new_file_path}")
         # open dialog and pass new_file_path
-        os.system("flet run gui_dialog.py")
-
-    def on_moved(self, e):
-        pass
-
-    def on_deleted(self, e):
-        pass
-
-def watch(path):
-    observer = Observer()
-    observer.schedule(EventHandler(), path=DIR, recursive=True)
-    observer.start()
-    while True:
-        time.sleep(1)
+        path = filedialog.askdirectory()
+        print(path)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-d", "--dir")
     args = parser.parse_args()
     DIR = args.dir
-    
-    watch_thread = threading.Thread(target=watch, args=(DIR,))
-    watch_thread.start()
+
+    root = tk.Tk()   
+    root.geometry("0x0")
+    root.withdraw()
+
+    observer = Observer()
+    observer.schedule(EventHandler(root), path=DIR, recursive=True)
+    observer.start()
+
+    try:
+        root.mainloop()
+    except KeyboardInterrupt:
+        observer.stop()
+    observer.join()
