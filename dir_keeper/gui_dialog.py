c6a4da08e06e806400e04d40af628c619c4b4db6
oratiek <oratiek522@gmail.com>
2023-12-17 20:54:55 +0900

    ファイルダイアログだけを出すようにした
    GUIフレームワークをtkinterに変えた

--- dir_keeper/gui_dialog.py
+++ dir_keeper/gui_dialog.py
@@ -1,38 +1,18 @@
-import flet as ft
+from tkinter import filedialog, Tk
+import platform
 
-def dialog(page: ft.Page):
-    # window config
-    page.window_height = 100
-    page.window_width = 210
-    #page.window_frameless = True 
-    page.window_resizable = False
+root = Tk()
+root.geometry("0x0")
+root.overrideredirect(1)
+root.withdraw()
 
-    def pick_files_result(e: ft.FilePickerResultEvent):
-        print(e.path)
-
-    def __open():
-        pick_files_dialog.get_directory_path()
-
-
-    pick_files_dialog = ft.FilePicker(on_result=pick_files_result)
-    selected_files = ft.Text()
-
-    page.overlay.append(pick_files_dialog)
-
-    page.add(
-        ft.Row(
-            [
-                ft.ElevatedButton(
-                    "Choose Directory",
-                    icon = ft.icons.UPLOAD_FILE,
-                    on_click = lambda _:pick_files_dialog.get_directory_path()
-                ),
-                selected_files,
-            ]
-        )
-    )
-    __open()
-
+def select_file():
+    root.update()
+    root.lift()
+    root.focus_force()
+    path = filedialog.askopenfilename()
+    root.update()
+    print(path)
 
 if __name__ == "__main__":
-    ft.app(target=dialog)
+    select_file()
